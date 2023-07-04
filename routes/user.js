3c88c70cd3a019fd893b0c60868a49f48beb1626
오승원 <neteakasa@gmail.com>
2023-07-04 10:53:52 +0900

    modified:   routes/index.js 	modified:   routes/user.js 	modified:   server.js

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,29 @@
 const express = require('express')
+const db_config = require('../config/database')
+const oracledb = require('oracledb')
 
 const router = express.Router()
+
+const connPromise = db_config.connect();
+
+router.get('/join', async (req, res) => {
+    const conn = await connPromise;
+
+    let sql = 'select * from tb_member'
+
+    conn.execute(sql, [], (err, { rows }) => {
+        if (err) {
+            throw err
+        };
+        console.log(rows);
+        conn.release((err) => {
+            if (err) { throw err; }
+            console.log('연결해제');
+        })
+
+    })
+
+})
 
 
 
