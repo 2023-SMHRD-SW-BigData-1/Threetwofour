704da85f8b6ecf8a842be0315e4eafeb0c3a2c13
seoyeoon <edusyj71@gmail.com>
2023-07-18 11:21:39 +0900

    modified:   react-project/src/App.js 	modified:   react-project/src/page/BowlingAlley.jsx 	modified:   react-project/src/page/MatchForm.jsx

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -35,7 +35,8 @@ function App(data) {
           <Route exact path='/community/*' element={<Community /> } />
           <Route path='/user/match' element={<MatchForm />} />
           <Route path='/user/mypage' element={<MyPage />} />
-          <Route path='/user/BowlingAlley' element={<BowlingAlley />} />
+          <Route path='/user/BowlingAlley/' element={<BowlingAlley />} />
+          <Route path='/user/BowlingAlley/:addr' element={<BowlingAlley />} />
           
         </Routes>
       </div>
