815ece275083a5dae8d1924a2abb27e0896e9aa5
seoyeoon <edusyj71@gmail.com>
2023-07-20 09:58:38 +0900

    modified:   react-project/src/components/Footer.jsx

--- react-project/src/components/Footer.jsx
+++ react-project/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="footer">
             <div>SPARECITY</div>
             <div>
-                스마트인재개발원 324팀<br />
+                스마트인재개발원 300팀<br />
                 팀장 조서연<br />
                 팀원 오승원 홍경표 박지원
             </div>
